c139e9ecf45be56068f981ace96a148aaae7fc26
lluys <lluis.martinez01@goumh.umh.es>
2019-04-02 21:11:21 +0200

    Reserva y liberacion de espacio para almacenar los datos de los sensores

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void simular(char* nombre, int sensores, int lecturas){
 	FILE *f;
 	int i, j;
 	f = fopen(nombre, "w");
-    printf("Hola");
+    
 	for(j=0;j<sensores;j++){
 		fprintf(f, "S%d\t\t", j);
 	}
@@ -31,23 +31,29 @@ void simular(char* nombre, int sensores, int lecturas){
 
 int main(int argc, const char* argv[]){
     int i, j;
-    
+    int **datosSensores;    //Estructura para guardar los datos leidos de los sensores
   int num_sensores = atoi(argv[1]);
   int num_lecturas = atoi(argv[3]);
   int tiempo_lectura = atoi(argv[2]);
-  printf("Hola");
+  
   simular("sensores.txt", num_sensores, num_lecturas);
 
-  printf("Datos de entrada sensores:%d, lecturas:%d, frecuencias: %d", num_sensores, num_lecturas, tiempo_lectura);
+  //printf("Datos de entrada sensores:%d, lecturas:%d, frecuencias: %d", num_sensores, num_lecturas, tiempo_lectura);
 
-	int **datosSensores;
+
 	datosSensores = (int **)malloc(num_lecturas*sizeof(int*));
 	for (i=0;i<num_sensores;i++)
 	{
-		
+		datosSensores[i] = (int *)malloc(num_lecturas*sizeof(int));
 	}
 
-  
+
+
+	for (i=0;i<num_sensores;i++)
+	{
+		free(datosSensores[i]);
+	}  
+    free(datosSensores);
   //Esta linea para que el Travis no nos borre nada al compilar
   printf("\n\n");
   fflush(stdout);
