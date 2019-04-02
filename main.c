557042099ec3247b4e14318d22e3da49e619b0b5
xJorgEx <jorge.gonzalez11@goumh.umh.es>
2019-04-02 18:24:23 +0200

    funcion leer

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void leer()
+{
+	
+}
 
 int main(int argc, const char* argv[]){
   int num_sensores = atoi(argv[1]);
   int num_lecturas = atoi(argv[3]);
   float tiempo_lectura = atof(argv[2]);
-  
+
   printf("Datos de entrada sensores:%d, lecturas:%d, frecuencias: %f", num_sensores, num_lecturas, tiempo_lectura);
   
-  
+	int **datosSensores;
+	datosSensores = (int **)malloc(num_lecturas*sizeof(int*));
+	for ()
+	{
+		
+	}
   
   //Esta linea para que el Travis no nos borre nada al compilar
   printf("\n\n");
