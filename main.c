e3a24d28bf991021671e5d004abdc845a97f1d1a
lluys <lluis.martinez01@goumh.umh.es>
2019-04-02 18:16:56 +0200

    Finalizada funcion simular (Punto 1 practica)

--- main.c
+++ main.c
@@ -1,14 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void simular(char* nombre, int sensores, int lecturas){
+	FILE *f;
+	int i, j;
+	f = fopen(nombre, "w");
+
+	for(j=0;j<sensores;j++){
+		fprintf(f, "S%d\t", j);
+	}
+	
+
+
+	for(i=0;i<lecturas;i++){
+		fprintf(f, "\n");
+		for(j=0;j<sensores;j++){
+			fprintf(f, "%f\t", atof(rand())/atof(MAX_RAND));
+		}
+	}	
+	fprintf(f, "\n");	
+
+}
+
 
 int main(int argc, const char* argv[]){
   int num_sensores = atoi(argv[1]);
   int num_lecturas = atoi(argv[3]);
-  float tiempo_lectura = atof(argv[2]);
+  int tiempo_lectura = atoi(argv[2]);
   
-  printf("Datos de entrada sensores:%d, lecturas:%d, frecuencias: %f", num_sensores, num_lecturas, tiempo_lectura);
+	simular("sensores.txt", num_sensores, num_lecturas);
+
+  printf("Datos de entrada sensores:%d, lecturas:%d, frecuencias: %d", num_sensores, num_lecturas, tiempo_lectura);
   
+
+
   
   
   //Esta linea para que el Travis no nos borre nada al compilar
