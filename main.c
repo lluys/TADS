83e6871f0b99922a46f46f3b9dba8b12bfef5cbd
lluys <lluis.martinez01@goumh.umh.es>
2019-04-13 01:47:22 +0200

    Version 1.0 totalmente finalizada

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
+#include <string.h>
 
-void simular(char* nombre, int sensores){
+void simular(int* datos, char* nombre, int sensores){
 	FILE *f;
 	int j;
 	f = fopen(nombre, "w");
     
 	
 	for(j=0;j<sensores;j++){
-		fprintf(f, "%d\t", rand()%100);
+		datos[j] = rand()%100;
+		fprintf(f, "%d\t", datos[j]);
 	}
 	
 	fprintf(f, "\n");	
@@ -20,23 +23,67 @@ void simular(char* nombre, int sensores){
 
 
 int main(int argc, const char* argv[]){
-    int i, j;
+
+    int i, j, contador;
+
+	int n_aleatorio;
+
 	FILE *f;
 	clock_t tini;
  	int num_sensores = atoi(argv[1]);
 	int num_lecturas = atoi(argv[3]);
 	int tiempo_lectura = atoi(argv[2]);
-	
+	int *datos_sensores, *sensores_leidos;
+	bool reinicio_busqueda;
 	//printf("Datos de entrada sensores:%d, lecturas:%d, frecuencias: %d", num_sensores, num_lecturas, tiempo_lectura);  
-		
+	
+	datos_sensores = (int*)malloc(sizeof(int)*num_sensores);
+	sensores_leidos = (int*)malloc(sizeof(int)*num_sensores);
+
+
+	
 	while(1){
-		tini = clock();
-		while(((double)(clock() - tini) / CLOCKS_PER_SEC) < tiempo_lectura){
-            //Tic Tac, no tenemos nueva lectura aun
-            //printf("Total time taken by CPU: %f\n", ((double)(clock() - tini) / CLOCKS_PER_SEC)  );
-		}
-		simular("sensores.txt", 5);
-	}
+		simular(datos_sensores, "sensores.txt", 5);
+		
+		for(i=0;i<num_sensores;i++){
+            sensores_leidos[i] = 0;
+        }
+		
+		contador = 0;
+		while(contador<num_sensores){
+			tini = clock();
+			while(((double)(clock() - tini) / CLOCKS_PER_SEC) < tiempo_lectura){
+				//Tic Tac, no tenemos nueva lectura aun
+				//printf("Total time taken by CPU: %f\n", ((double)(clock() - tini) / CLOCKS_PER_SEC)  );
+			}
+			reinicio_busqueda = false;
+			n_aleatorio = rand()%num_sensores;
+				
+			while(sensores_leidos[n_aleatorio] != 0){
+                
+                n_aleatorio++;
+               
+				//Volvemos al principio
+				if(n_aleatorio==num_sensores){
+                    
+                    if(!reinicio_busqueda){
+                        n_aleatorio=0;   
+                        reinicio_busqueda = true;
+                        
+                    }else{
+    					//Se supone que nunca llegaremos a este punto
+    					//Si pasa ha habido algun error
+    					return 1;
+                    }
+                    
+            }
+			}//Hemos encontrado un sensor por leer
+			sensores_leidos[n_aleatorio]++;
+			
+			contador++;
+		}//Salimos de las lecturas
+		printf("Ya los hemos leido todos");
+	}//fin bucle infinito
     
   //Esta linea para que el Travis no nos borre nada al compilar
   printf("\n\n");
