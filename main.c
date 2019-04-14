68e44d1fef8af06add89a0543be5d86256d01ebf
lluys <lluis.martinez01@goumh.umh.es>
2019-04-14 21:43:36 +0200

    Ordenacion de valores de mayor a menor. Punto 4 finalizado

--- main.c
+++ main.c
@@ -1,11 +1,76 @@
 #include "Estructuras.h"
 
+//Solo estructura, no se ha implementeado correctamente aun
+void ordenarBurbuja(t_lectura* datos, int num_sensores){
+    int i, j;
+    t_lectura temp;
+    for(i=0;i<num_sensores;i++){
+            printf("%s: %d\n", datos[i].etiqueta, datos[i].valor);
+        }
+    for (i=1;i<num_sensores;i++)
+    {
+       for (j=0; j < num_sensores-i ;j++) // for(j=0; j < Nelementos-i; j++) es menor y no menor igual
+       {
+          if (datos[j].valor > datos[j+1].valor)//Condicion mayor-menor
+          {
+            temp=datos[j];
+            datos[j]=datos[j+1];
+            datos[j+1]=temp;
+          }
+       }
+    }
+    
+}
 
 void etiquetar(t_lectura datos, int sensor){
     strcpy(datos.etiqueta, "S");
     //printf("%s\n", datos.etiqueta);
     itoa(sensor, datos.etiqueta+1, 10);
-    //printf("%s\n", datos.etiqueta);
+    printf("%s\n", datos.etiqueta);
+}
+
+
+
+int leerDatos(t_lectura* datos_sensores, int num_sensores, int num_lecturas, int tiempo_lectura){
+    int *sensores_leidos;
+    int n_aleatorio, i, j, contador;
+    bool reinicio_busqueda;
+    clock_t tini;
+    
+    sensores_leidos = (int*)malloc(sizeof(int)*num_sensores);
+    for(i=0;i<num_sensores;i++){
+        sensores_leidos[i] = 0;
+    }
+    
+    contador = 0;
+	while(contador<num_sensores){
+		tini = clock();
+		while(((double)(clock() - tini) / CLOCKS_PER_SEC) < tiempo_lectura){
+			//Tic Tac, no tenemos nueva lectura aun
+			//printf("Total time taken by CPU: %f\n", ((double)(clock() - tini) / CLOCKS_PER_SEC)  );
+		}
+		reinicio_busqueda = false;
+		n_aleatorio = rand()%num_sensores;
+			
+		while(sensores_leidos[n_aleatorio] != 0){ 
+            n_aleatorio++;
+			//Volvemos al principio
+			if(n_aleatorio==num_sensores){
+                if(!reinicio_busqueda){
+                    n_aleatorio=0;   
+                    reinicio_busqueda = true;
+                }else{
+					//Se supone que nunca llegaremos a este punto
+					//Si pasa ha habido algun error
+					return 0;
+                }
+            }
+		}//Hemos encontrado un sensor por leer
+		sensores_leidos[n_aleatorio]++;
+		
+		contador++;
+	}//Salimos de las lecturas
+	return 1;
 }
 
 void simular(t_lectura* datos, char* nombre, int sensores){
@@ -18,7 +83,7 @@ void simular(t_lectura* datos, char* nombre, int sensores){
 		datos[j].valor = rand()%100;
 		etiquetar(datos[j], j);
 		
-		//printf("datos[j].valor: %d", datos[j].valor);
+		printf("datos[j].valor: %d\n", &datos[j].valor);
 		fprintf(f, "%d\t", datos[j].valor);
 	}
 	
@@ -28,67 +93,37 @@ void simular(t_lectura* datos, char* nombre, int sensores){
 }
 
 
-
 int main(int argc, const char* argv[]){
-    int i, j, contador;
-	int n_aleatorio;
-
+    int i, j;
 	FILE *f;
-	clock_t tini;
+	
  	int num_sensores = atoi(argv[1]);
 	int num_lecturas = atoi(argv[3]);
 	int tiempo_lectura = atoi(argv[2]);
 	t_lectura *datos_sensores;
-	int *sensores_leidos;
-	bool reinicio_busqueda;
+	
 	//printf("Datos de entrada sensores:%d, lecturas:%d, frecuencias: %d", num_sensores, num_lecturas, tiempo_lectura);  
 	
 	datos_sensores = (int*)malloc(sizeof(t_lectura)*num_sensores);
-	sensores_leidos = (int*)malloc(sizeof(int)*num_sensores);
-
-
 	
 	while(1){
-		simular(datos_sensores, "sensores.txt", 5);
+		simular(datos_sensores, "sensores.txt", num_sensores);
+		printf("aaaaaaaaaaaaaa%s\n", datos_sensores[0].etiqueta);
 		
+		if(!leerDatos(datos_sensores, num_sensores, num_lecturas, tiempo_lectura)){
+            return -1;
+        }
+        printf("\n\n");
+        for(i=0;i<num_sensores;i++){
+            printf("%s: %d\n", datos_sensores[i].etiqueta, datos_sensores[i].valor);
+        }
+        ordenarBurbuja(datos_sensores, num_sensores);
+		printf("\n\n");
 		for(i=0;i<num_sensores;i++){
-            sensores_leidos[i] = 0;
+            printf("%s: %d\n", datos_sensores[i].etiqueta, datos_sensores[i].valor);
         }
 		
-		contador = 0;
-		while(contador<num_sensores){
-			tini = clock();
-			while(((double)(clock() - tini) / CLOCKS_PER_SEC) < tiempo_lectura){
-				//Tic Tac, no tenemos nueva lectura aun
-				//printf("Total time taken by CPU: %f\n", ((double)(clock() - tini) / CLOCKS_PER_SEC)  );
-			}
-			reinicio_busqueda = false;
-			n_aleatorio = rand()%num_sensores;
-				
-			while(sensores_leidos[n_aleatorio] != 0){
-                
-                n_aleatorio++;
-               
-				//Volvemos al principio
-				if(n_aleatorio==num_sensores){
-                    
-                    if(!reinicio_busqueda){
-                        n_aleatorio=0;   
-                        reinicio_busqueda = true;
-                        
-                    }else{
-    					//Se supone que nunca llegaremos a este punto
-    					//Si pasa ha habido algun error
-    					return 1;
-                    }
-                    
-            }
-			}//Hemos encontrado un sensor por leer
-			sensores_leidos[n_aleatorio]++;
-			
-			contador++;
-		}//Salimos de las lecturas
-		printf("Ya los hemos leido todos");
+		printf("Ya los hemos leido todos\n");
 	}//fin bucle infinito
     
   //Esta linea para que el Travis no nos borre nada al compilar
