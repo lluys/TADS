239e92a4f9ee8aa38ccf66ce6ffb360ba2ea4734
lluys <lluis.martinez01@goumh.umh.es>
2019-04-15 18:45:13 +0200

    He escrito unas lineas

---
+++ servidor/main.c
@@ -0,0 +1,24 @@
+#include "Estructuras.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+
+
+int main(int argc, const char* argv[]){
+    int i, j;
+	
+	while(1){
+		
+		
+
+	}//fin bucle infinito
+    
+  //Esta linea para que el Travis no nos borre nada al compilar
+  printf("\n\n");
+  fflush(stdout);
+  return 0;
+}
