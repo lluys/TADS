eb136695ac1c165a2bce61cffc3a61ae54280126
XwJorgEwX <jorge.gonzalez11@goumh.umh.es>
2019-04-15 21:43:14 +0200

    Añadida caracteristica al servidor (devolver alertas segun valores)

--- servidor/main.c
+++ servidor/main.c
@@ -10,12 +10,36 @@
 
 int main(int argc, const char* argv[]){
     int i, j;
+    t_lectura *datos_sensores;
+    int num_sensores = atoi(argv[1]);
 	
-	while(1){
-		
-		
-
-	}//fin bucle infinito
+	datos_sensores = (t_lectura*)malloc(sizeof(t_lectura)*num_sensores);
+    
+    int valor;
+    int etiqueta;
+    double tiempo;
+    
+    while(1)
+    {
+        FILE* archivo;
+        archivo = fopen("envio.txt", "r");
+        
+        if(archivo == NULL)
+        {
+            return-1;
+        }
+        
+        while(!feof(archivo))
+        {
+            fscanf(archivo, "%d", &valor);
+            fscanf(archivo, "%d", &etiqueta);
+            fscanf(archivo, "%f", &tiempo);
+            
+            datos_sensores[etiqueta-1].valor = valor;
+            datos_sensores[etiqueta-1].etiqueta = etiqueta;
+            datos_sensores[etiqueta-1].momento = tiempo;
+        }
+	}//fin bucle
     
   //Esta linea para que el Travis no nos borre nada al compilar
   printf("\n\n");
